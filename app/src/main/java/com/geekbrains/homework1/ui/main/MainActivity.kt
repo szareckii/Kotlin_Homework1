a465f46658f43542442aa5ecd87bbe5764e36399
szareckii <zareckii@gmail.com>
2020-10-12 00:08:25 +0300

    HomeWork 1 1. Java to Kotlin 2. MVVM

---
+++ app/src/main/java/com/geekbrains/homework1/ui/main/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.geekbrains.homework1.ui.main
+
+import android.os.Bundle
+import android.widget.GridLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.geekbrains.homework1.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+    lateinit var adapter: NotesRVAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        rv_notes.layoutManager = GridLayoutManager(this, 2)
+        adapter = NotesRVAdapter()
+        rv_notes.adapter = adapter
+
+        viewModel.viewState().observe(this, {value ->
+            value?.let { adapter.notes = it.notes }
+        })
+    }
+}
